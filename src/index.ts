5632383d13e073d6df2998244b0d35fc5d3ba656
cojmeister <luqas.c@gmail.com>
2022-11-23 15:38:26 +0200

    Added documentation

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,14 @@ type CallbackFunction = (
   res?: string | EventEmitter
 ) => any | void;
 
+/**
+ * Compress inputFile by compLevel to outputFile using zstd
+ * @param {string} inputFile Path to the to file to be compressed - can be relative or absolute
+ * @param {string} outputFile Path to store the resulting compressed file - can be relative or absolute
+ * @param {number} [compLevel=3] Compression - int between 1 and 22; [default=3]
+ * @param {CallbackFunction} [callback] Optional Callback Function to be executed on task completion. Follows Node.js (err, result) pattern, being result equal to outputFile when succeeded.
+ * @returns {void}None
+ */
 export function compress(
   inputFile: string,
   outputFile: string,
@@ -72,6 +80,16 @@ export function compress(
   });
 }
 
+/**
+ * Compress stream by compLevel to outputFile using zstd
+ * @param {fs.ReadStream} readableStream Data Node.js Stream to be compressed.
+ * @param {string} outputFile Path to store the resulting compressed file - can be relative or absolute. Output file is overwritten if it exists.
+ * @param {number} [compLevel=3] Compression - int between 1 and 22; [default=3]
+ * @param {CallbackFunction} [callback] Optional Callback Function to be executed on task initialization. Follows Node.js (err, result) pattern, being result an EventEmitter that can emit the following events:
+ *  - error: Emitted when an error reading the stream or creating the compressed file occurred. Error message is provided.
+ *  - end: Emitted when task ended successfully.
+ * @returns {void}None
+ */
 export function compressStreamToFile(
   readableStream: fs.ReadStream,
   outputFile: string,
@@ -126,6 +144,14 @@ export function compressStreamToFile(
   });
 }
 
+/**
+ * Decompress inputFile to outputFile using zstd - call callback function upon completion
+ * * NOTE: Input and output files should be different. Output file is overwritten if it exists.
+ * @param {string} inputFile Path to the to compressed input file.
+ * @param {string} outputFile Path to store the resulting decompressed file.
+ * @param {CallbackFunction} [callback] Function to be executed on task completion. Follows Node.js (err, result) pattern, being result equal to outputFile when succeeded.
+ * @returns {void}None
+ */
 export function decompress(
   inputFile: string,
   outputFile: string,
@@ -185,6 +211,15 @@ export function decompress(
   });
 }
 
+/**
+ * Decompress inputFile to stream on callback using zstd
+ * @param {string} inputFile Path to the to compressed input file.
+ * @param {CallbackFunction} [callback] Function to be executed on task initialization. Follows Node.js (err, result) pattern, being result an EventEmitter that can emit the following events:
+ * - error: Emitted when an error involving streams or file decompression occurred. Error message is provided.
+ * - data: Emitted when a chunk of decompressed data is generated. Data is obviously provided.
+ * - end: Emitted when decompression ended and all data events have been emitted.
+ * @returns {void}None
+ */
 export function decompressionStreamFromFile(
   inputFile: string,
   callback?: CallbackFunction
@@ -240,6 +275,15 @@ export function decompressionStreamFromFile(
   });
 }
 
+/**
+ * Decompress inputFile to stream using zstd -  get output on callback with eventEmitter
+ * @param {string}inputFile Path to the to compressed input file.
+ * @param {fs.WriteStream}writableStream Node.js Stream where to output decompressed data.
+ * @param {CallbackFunction}[callback] Function to be executed on task initialization. Follows Node.js (err, result) pattern, being result an EventEmitter that can emit the following events:
+ * - error: Emitted when an error involving streams or file decompression occurred. Error message is provided.
+ * - finish: Emitted when finished writting decompressed data to output stream.
+ * @returns {void}None
+ */
 export function decompressFileToStream(
   inputFile: string,
   writableStream: fs.WriteStream,
@@ -249,12 +293,16 @@ export function decompressFileToStream(
   const eventEmitter = new events.EventEmitter();
   fs.access(inputFile, fs.constants.R_OK, (err) => {
     if (err) {
-      if (callback) callback(new Error("Input file is not readable."));
+      if (callback) {
+        callback(new Error("Input file is not readable."));
+      }
       return;
     }
     fs.access(zstdBinPath, fs.constants.R_OK | fs.constants.X_OK, (err) => {
       if (err) {
-        if (callback) callback(new Error("Zstd binary is not executable."));
+        if (callback) {
+          callback(new Error("Zstd binary is not executable."));
+        }
         return;
       }
 
--- types/index.d.ts
+++ types/index.d.ts
@@ -3,10 +3,54 @@
 import { default as EventEmitter } from "events";
 import * as fs from "fs";
 type CallbackFunction = (err: Error | null, res?: string | EventEmitter) => any | void;
+/**
+ * Compress inputFile by compLevel to outputFile using zstd
+ * @param {string} inputFile Path to the to file to be compressed - can be relative or absolute
+ * @param {string} outputFile Path to store the resulting compressed file - can be relative or absolute
+ * @param {number} [compLevel=3] Compression - int between 1 and 22; [default=3]
+ * @param {CallbackFunction} [callback] Optional Callback Function to be executed on task completion. Follows Node.js (err, result) pattern, being result equal to outputFile when succeeded.
+ * @returns {void}None
+ */
 export declare function compress(inputFile: string, outputFile: string, compLevel?: number, callback?: CallbackFunction): void;
+/**
+ * Compress stream by compLevel to outputFile using zstd
+ * @param {fs.ReadStream} readableStream Data Node.js Stream to be compressed.
+ * @param {string} outputFile Path to store the resulting compressed file - can be relative or absolute. Output file is overwritten if it exists.
+ * @param {number} [compLevel=3] Compression - int between 1 and 22; [default=3]
+ * @param {CallbackFunction} [callback] Optional Callback Function to be executed on task initialization. Follows Node.js (err, result) pattern, being result an EventEmitter that can emit the following events:
+ *  - error: Emitted when an error reading the stream or creating the compressed file occurred. Error message is provided.
+ *  - end: Emitted when task ended successfully.
+ * @returns {void}None
+ */
 export declare function compressStreamToFile(readableStream: fs.ReadStream, outputFile: string, compLevel?: number, callback?: CallbackFunction): void;
+/**
+ * Decompress inputFile to outputFile using zstd - call callback function upon completion
+ * * NOTE: Input and output files should be different. Output file is overwritten if it exists.
+ * @param {string} inputFile Path to the to compressed input file.
+ * @param {string} outputFile Path to store the resulting decompressed file.
+ * @param {CallbackFunction} [callback] Function to be executed on task completion. Follows Node.js (err, result) pattern, being result equal to outputFile when succeeded.
+ * @returns {void}None
+ */
 export declare function decompress(inputFile: string, outputFile: string, callback?: CallbackFunction): void;
+/**
+ * Decompress inputFile to stream on callback using zstd
+ * @param {string} inputFile Path to the to compressed input file.
+ * @param {CallbackFunction} [callback] Function to be executed on task initialization. Follows Node.js (err, result) pattern, being result an EventEmitter that can emit the following events:
+ * - error: Emitted when an error involving streams or file decompression occurred. Error message is provided.
+ * - data: Emitted when a chunk of decompressed data is generated. Data is obviously provided.
+ * - end: Emitted when decompression ended and all data events have been emitted.
+ * @returns {void}None
+ */
 export declare function decompressionStreamFromFile(inputFile: string, callback?: CallbackFunction): void;
+/**
+ * Decompress inputFile to stream using zstd -  get output on callback with eventEmitter
+ * @param {string}inputFile Path to the to compressed input file.
+ * @param {fs.WriteStream}writableStream Node.js Stream where to output decompressed data.
+ * @param {CallbackFunction}[callback] Function to be executed on task initialization. Follows Node.js (err, result) pattern, being result an EventEmitter that can emit the following events:
+ * - error: Emitted when an error involving streams or file decompression occurred. Error message is provided.
+ * - finish: Emitted when finished writting decompressed data to output stream.
+ * @returns {void}None
+ */
 export declare function decompressFileToStream(inputFile: string, writableStream: fs.WriteStream, callback?: CallbackFunction): void;
 export {};
 //# sourceMappingURL=index.d.ts.map
